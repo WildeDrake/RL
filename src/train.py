d0639faab2650984fa2134175df5f85b26c3faad
WildeDrake <diegovenegasanabalon@gmail.com>
2025-12-17 19:21:56 -0300

    Mejoras en Parametros .ini

--- src/train.py
+++ src/train.py
@@ -11,25 +11,27 @@ from trainingLoops import DQN_train_loop, PPO_train_loop
 
 def train(config_data, agent_type):
     # Cargar parametros de configuracion
-    env_name = config_data.get('env')                                   # Nombre del entorno.
-    eps_start = float(config_data.get('eps_start'))                     # Valor inicial de epsilon.
-    eps_end = float(config_data.get('eps_end'))                         # Valor final de epsilon.
-    eps_decay = float(config_data.get('eps_decay'))                     # Tasa de decaimiento de epsilon.
-    memory_size = int(config_data.get('memory_size'))                   # Tamaño de la memoria de experiencia.
-    learning_rate = float(config_data.get('learning_rate'))             # Tasa de aprendizaje.
-    initial_memory = int(config_data.get('initial_memory'))             # Memoria inicial antes de entrenar.
-    gamma = float(config_data.get('gamma'))
-    gae_lambda = float(config_data.get('gae_lambda'))
-    clip_ratio = float(config_data.get('clip_ratio'))                   # Factor de descuento.
-    target_update = int(config_data.get('target_update'))               # Frecuencia de actualizacion de la red objetivo.
-    batch_size = int(config_data.get('batch_size'))                     # Tamaño del lote para el entrenamiento.
-    model_path = config_data.get('model_path')
-    epochs = int(config_data.get('epochs'))                             # Número de épocas para PPO.
-    episodes = int(config_data.get('episodes'))                         # Número de episodios.
-    max_episode_length = int(config_data.get('max_episode_length'))     # Longitud maxima de un episodio.
-    save_model_interval = int(config_data.get('save_model_interval'))   # Habilitar Prioritized Experience Replay
-    run_name = config_data.get('run_name', fallback='default_run')      # Intervalo para guardar el modelo.
-    '''---------------------------------------- Rainbow DQN----------------------------------------'''
+    '''---------------------------------------- PARAMETROS DE CONFIGURACION GENERALES----------------------------------------'''
+    env_name = config_data.get('env')                                                # Nombre del entorno..
+    model_path = config_data.get('model_path')                                       # Ruta para guardar/cargar el modelo.  
+    run_name = config_data.get('run_name')                                           # Directorio para guardar graficos del train.
+    save_model_interval = int(config_data.get('save_model_interval', fallback=500))  # Intervalo de guardado del modelo.
+    '''---------------------------------------- PARAMETROS DE AGENTES----------------------------------------'''
+    eps_start = float(config_data.get('eps_start', fallback=1))                     # DQN: Valor inicial de epsilon.
+    eps_end = float(config_data.get('eps_end', fallback=0.01))                      # DQN: Valor final de epsilon.
+    eps_decay = float(config_data.get('eps_decay', fallback=8000))                  # DQN: Tasa de decaimiento de epsilon.
+    memory_size = int(config_data.get('memory_size', fallback=100000))              # DQN: Tamaño de la memoria de experiencia.
+    learning_rate = float(config_data.get('learning_rate', fallback=0.0001))        # DQN: Tasa de aprendizaje.
+    initial_memory = int(config_data.get('initial_memory', fallback=10000))         # DQN: Memoria inicial antes de entrenar.
+    gamma = float(config_data.get('gamma', fallback=0.99))                          # DQN: Factor de descuento.
+    target_update = int(config_data.get('target_update', fallback=2000))            # DQN: Frecuencia de actualizacion de la red objetivo.
+    max_episode_length = int(config_data.get('max_episode_length', fallback=2000))  # DQN: Longitud maxima de un episodio.
+    episodes = int(config_data.get('episodes', fallback=10000))                     # DQN: Número de episodios.
+    batch_size = int(config_data.get('batch_size', fallback=128))                 # DQN Y PPO: Tamaño del lote para el entrenamiento
+    gae_lambda = float(config_data.get('gae_lambda', fallback=0.95))            # PPO: Lambda para GAE (PPO).
+    clip_ratio = float(config_data.get('clip_ratio', fallback=0.1))             # PPO: Factor de descuento.              
+    epochs = int(config_data.get('epochs', fallback=4))                         # PPO: Número de épocas para PPO.
+    '''---------------------------------------- PARAMETROS DE RAINBOW DQN----------------------------------------'''
     use_double = config_data.getboolean('use_double', fallback=False)                   # Habilitar Double DQN.
     use_dueling = config_data.getboolean('use_dueling', fallback=False)                 # Habilitar Dueling DQN.
     use_per = config_data.getboolean('use_per', fallback=False)                         # Habilitar Prioritized Experience Replay   
